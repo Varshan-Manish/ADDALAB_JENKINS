38b3383f306dd3fb49211b787ebd658e205f5fa0
MIC0101 <118362659+Varshan-Manish@users.noreply.github.com>
2024-10-04 20:20:38 +0530

    Create Jenkins22MIC0101.java

---
+++ Jenkins22MIC0101.java
@@ -0,0 +1,6 @@
+public class Jenkins22MIC0101{
+  public static void main(String[] args){
+    System.out.println("Program By 22MIC0101");
+    System.out.println("Hello World Jenkins Build Automation Working");
+  }
+}
