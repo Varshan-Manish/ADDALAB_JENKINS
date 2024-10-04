044def757c3bdcf585aec0ee91d001e3a86fd0fe
Varshan-Manish <varshan.manish2022@vitstudent.ac.in>
2024-10-04 20:03:31 +0530

    First Commit

---
+++ src/pkg22mic0101jenkins/Main.java
@@ -0,0 +1,8 @@
+package pkg22mic0101jenkins;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("22MIC0101");
+        System.out.println("Hello World");
+    }
+    
+}
